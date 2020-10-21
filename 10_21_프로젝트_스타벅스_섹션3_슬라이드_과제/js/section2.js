2be631e257384161629c7b2dcf3713932a726089
Elly Sohye Park <63501197+ps9610@users.noreply.github.com>
2020-10-21 09:48:38 +0900

    21일 과제

--- 10_21_프로젝트_스타벅스_섹션3_슬라이드_과제/js/section2.js
+++ 10_21_프로젝트_스타벅스_섹션3_슬라이드_과제/js/section2.js
@@ -6,16 +6,24 @@
 
     function noticeSlideFn(){
         cnt++; //증감수라서 처음이 -1로 시작해야 0부터 나옴
-        if(cnt > 4){
-            cnt = 0;
+        if(cnt > 3){
+            cnt = -1; //마지막 슬라이드를 0으로 셋팅, (마지막이전 마지막)
         }
         $(".notice-list li").stop().animate({top:24/* css에서 li top값이 24니까} */},0).css({zIndex:2}); //롤링 대상이 되는 요소를 앞에 쓰면됨
         //👆 모두 초기화 24픽셀 아래에서 대기
-        $(".notice-list li").eq(cnt).stop().animate({top:0},0)/* 기다리는 요소들은 무조건 0초해야 일시정지함 */.css({zIndex:1}); //롤링 대상이 되는 요소를 앞에 쓰면됨
+        $(".notice-list li").eq(cnt<0? 4:cnt).stop().animate({top:0},0)/* 기다리는 요소들은 무조건 0초해야 일시정지함 */.css({zIndex:1}); //롤링 대상이 되는 요소를 앞에 쓰면됨
         //👆 첫번째만 화면에서 대기
-        $(".notice-list li").eq(cnt+1>4? 0:cnt).stop().animate({top:24},0)/* top 24에서 일시정지 */.animate({top:0},1000/* 올라오는데 걸리는 시간 1초 */).css({zIndex:3}); //롤링 대상이 되는 요소를 앞에 쓰면됨
+        $(".notice-list li").eq(cnt+1).stop().animate({top:24},0)/* top 24에서 일시정지 */.animate({top:0},1000/* 올라오는데 걸리는 시간 1초 */).css({zIndex:3}); //롤링 대상이 되는 요소를 앞에 쓰면됨
         //👆 24픽셀 아래에서 위로 부드럽게 올라온다.
     }
+
+//0 1
+//1 2
+//2 3
+//3 4
+
+
+//4 0
 
     /* 일단 수동으로 테스트해보기 :
     noticeSlideFn(); //0번 텍스트 1번 텍스트
